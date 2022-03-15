7ba4fb3550519cf71978058ee4a4563191f72552
K1-R1 <kyranrawlinson@gmail.com>
2022-03-15 21:49:51 +0000

    Updated src/App.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,11 @@ export default function App() {
     }
 
     function holdDice(id) {
-
+        setDice(oldDice => oldDice.map(die => {
+            return die.id === id ?
+                { ...die, isHeld: !die.isHeld } :
+                die
+        }))
     }
 
     const diceElements = dice.map(die => <Die value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)} />)
