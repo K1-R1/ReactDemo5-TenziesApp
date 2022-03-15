418696e317222534229a9ec6d78c2aaf33c6c1a7
K1-R1 <kyranrawlinson@gmail.com>
2022-03-15 21:57:26 +0000

    Added functionality to lock die values

--- src/App.js
+++ src/App.js
@@ -6,20 +6,28 @@ export default function App() {
 
     const [dice, setDice] = useState(allNewDice())
 
+    function generateNewDie() {
+        return {
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid()
+        }
+    }
+
     function allNewDice() {
         const newDice = []
         for (let i = 0; i < 10; i++) {
-            newDice.push({
-                value: Math.ceil(Math.random() * 6),
-                isHeld: false,
-                id: nanoid()
-            })
+            newDice.push(generateNewDie())
         }
         return newDice
     }
 
     function rollDice() {
-        setDice(allNewDice())
+        setDice(oldDice => oldDice.map(die => {
+            return die.isHeld ?
+                die :
+                generateNewDie()
+        }))
     }
 
     function holdDice(id) {
@@ -34,6 +42,8 @@ export default function App() {
 
     return (
         <main>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click a die to lock its value.</p>
             <div className="dice-container">
                 {diceElements}
             </div>
