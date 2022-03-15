9409d0751f29aaa8d9fd35b470b2e3dac7c3cc83
K1-R1 <kyranrawlinson@gmail.com>
2022-03-15 21:16:48 +0000

    Updated src/App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import Die from "./component/Die"
 
 export default function App() {
+
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push(Math.ceil(Math.random() * 6))
+        }
+        return newDice
+    }
+
     return (
         <main>
             <div className="dice-container">
